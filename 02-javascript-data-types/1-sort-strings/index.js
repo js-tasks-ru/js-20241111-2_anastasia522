9d1274674712585a486f66ea2f19c8369a177d43
Анастасия Горлина <anastasiagorlina@Noutbuk-Anastasia.local>
2024-11-24 17:35:48 +0300

    task-2.1

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,12 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
-
+  const newArr = [...arr];
+  return newArr.sort((a, b) => {
+    if (param === 'asc') {
+      return a.localeCompare(b, ["ru-RU", "en-EN"], {caseFirst: 'upper'});
+    } else if (param === 'desc') {
+      return b.localeCompare(a, ["ru-RU", "en-EN"], {caseFirst: 'upper'});
+    }
+  });
 }
