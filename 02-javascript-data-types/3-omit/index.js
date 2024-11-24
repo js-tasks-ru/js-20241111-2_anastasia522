c1f0f4dbfd6a5544db91b82d071978fbcdda72fb
Анастасия Горлина <anastasiagorlina@Noutbuk-Anastasia.local>
2024-11-24 19:33:24 +0300

    task 2

--- 02-javascript-data-types/3-omit/index.js
+++ 02-javascript-data-types/3-omit/index.js
@@ -5,5 +5,11 @@
  * @returns {object} - returns the new object
  */
 export const omit = (obj, ...fields) => {
-
+  const result = {};
+  for (const [key, value] of Object.entries(obj)) {
+    if (!fields.includes(key)) {
+      result[key] = value;
+    }
+  }
+  return result;
 };
