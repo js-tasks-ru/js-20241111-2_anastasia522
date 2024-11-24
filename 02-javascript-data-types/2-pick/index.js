18ff3caeced15b0e8db3e56cf2f9556f5eee6b97
Анастасия Горлина <anastasiagorlina@Noutbuk-Anastasia.local>
2024-11-24 19:14:35 +0300

    task 2.2

--- 02-javascript-data-types/2-pick/index.js
+++ 02-javascript-data-types/2-pick/index.js
@@ -1,5 +1,11 @@
 
-const pick = (obj, ...fields) => {
+/**
+ * pick - Creates an object composed of the picked object properties:
+ * @param {object} obj - the source object
+ * @param {...string} fields - the properties paths to pick
+ * @returns {object} - returns the new object
+ */
+export const pick = (obj, ...fields) => {
   const result = Object.entries(obj).filter(([key, value]) => {
     if (fields.includes(key)) {
       return [key, value];
